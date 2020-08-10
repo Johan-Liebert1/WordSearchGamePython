cb88802523a28409ded3a9690500304c3f02becf
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2020-08-10 21:02:22 +0530

    Fixed show solution thingy

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ from words import all_words
 import random
 from termcolor import colored
 import pygame
+import copy
 
-white = (200, 200, 200)
+# Pygame Config
+white = (230, 230, 230)
 black = (0,0,0)
-pygame.font.init()
+red = (200, 0, 0)
 green = (0, 150, 0)
-FONT = pygame.font.SysFont("arial", 16)
 
+pygame.init()
+window = pygame.display.set_mode((600, 750))
+window.fill(white)
 
+pygame.font.init()
+FONT = pygame.font.SysFont("Times New Roman", 16)
+FONT_BIG = pygame.font.SysFont("Times New Roman", 24)
+
+run = True
+clock = pygame.time.Clock()
+
+words = []
+# Returns "number" randomly chosen words from the list in words.py
 def get_words(number):
-    words = []
     i = 0
 
     while i < number:
@@ -22,6 +34,7 @@ def get_words(number):
 
     return words
 
+'''Creates the boards of given dimension. Ex 15 x 15 or 10 x 10'''
 def create_board(dimension):
     board = []
 
@@ -30,6 +43,7 @@ def create_board(dimension):
 
     return board
 
+# Prints the board prettily
 def print_board(board):
     # board = dimension * dimension
     string = ''
@@ -45,6 +59,8 @@ def print_board(board):
 
 
 def add_words_to_board(board):
+    '''Calls function to add randomly chosen words to board'''
+
     # rollover allowed
 
     words = get_words(10)
@@ -147,14 +163,25 @@ def add(word, board, direction):
     
     return board
 
+'''fill the crossword'''
+def fill_crossword(crossword):
+    
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    filled_crossword = copy.deepcopy(crossword)
+    for row in range(len(crossword)):
+        for col in range(len(crossword)):
+            if filled_crossword[row][col] in letters:
+                continue
+            else:
+                filled_crossword[row][col] = letters[random.randrange(0, 26)]
+    return filled_crossword
+    
+   
+
 
 def main():
     crossword = add_words_to_board(board)
     return crossword
-
-pygame.init()
-window = pygame.display.set_mode((600, 600))
-window.fill(white)
 
 def create_grid():
     w = 600 // 15
@@ -167,6 +194,9 @@ def create_grid():
 
         y += w 
         x += w   
+    
+    pygame.draw.line(window, black, (620, 0), (620, width))
+    
 
 def put_letters_on_grid(crossword):
     x = 15
@@ -177,26 +207,61 @@ def put_letters_on_grid(crossword):
         x = 15
         for col in range(len(crossword)):
             x = 15 + 40 * (col)
-            if crossword[row][col] != '0':
-                text = FONT.render(crossword[row][col], 1, green)
-            else:
+            if crossword[row][col] == '0':
                 text = FONT.render(crossword[row][col], 1, black)
+            else:
+                text = FONT.render(crossword[row][col], 1, green)
 
             window.blit(text, (x,y))
 
-run = True
+    words_x = 0
+    words_y = 620
+
+    for i in range(len(words)):
+        wrd = " ".join(words[i])
+        text = FONT.render(wrd, 1, red)
+        window.blit(text, (words_x, words_y))
+        if words_x + text.get_width() + 10 < 600:
+            words_x += text.get_width() + 10
+
+        else:
+            words_x = 0
+            words_y += text.get_height() + 10
+
+    answer_text = FONT_BIG.render("Click here to see the solution", 1, red)
+    window.blit(answer_text, (150, 710))
+
+
 board = create_board(15)
 
 crossword = main()
 
+create_grid()
+
+filled_crossword = fill_crossword(crossword)
+put_letters_on_grid(filled_crossword)
+
 print_board(crossword)
 
+show_answer = False
+
 while run:
+    clock.tick(30)
+
+    #gets x and y position of mouse as (x, y)
+    mouse = pygame.mouse.get_pos()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    create_grid()
-    put_letters_on_grid(crossword)
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if 150 <= mouse[0] <= 150+283 and 710 <= mouse[1] <= 738:
+                window.fill(white)
+                create_grid()
+                put_letters_on_grid(crossword)
+
+  
     pygame.display.update()
 
 
